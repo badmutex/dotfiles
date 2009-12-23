50795a759ad205bf94a7ec0c563d8b1807a5f94b
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-12-23 13:51:57 -0500

    [xmonad] added layouts (dishes, spiral) and border toggle

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,5 +1,7 @@
 import XMonad hiding ((|||))
 
+import XMonad.Actions.NoBorders
+
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
@@ -11,8 +13,11 @@ import XMonad.Layout.Accordion
 import XMonad.Layout.LayoutCombinators
 import XMonad.Layout.NoBorders
 import XMonad.Layout.WindowArranger
+import XMonad.Layout.Dishes
+import XMonad.Layout.Spiral
 
 import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.EZConfig (additionalKeys)
 
 import XMonad.Config.Kde
 
@@ -25,18 +30,19 @@ import System.IO
 
 main = do
   xmproc <- spawnPipe "xmobar"
-  xmonad =<< xmobar 
-           myConfig {
-             modMask           = mod4Mask
-           , terminal          = myTerminal
-	   , borderWidth       = 1
-           , focusFollowsMouse = False
-           , workspaces        = map show [1..9]
-           , layoutHook        = myLayoutHook
-           , manageHook        = myManageHook
-           , startupHook       = myStartupHook
-           , logHook           = myXmobarPP xmproc
-           }
+  xmonad =<< xmobar (conf xmproc)
+
+conf xmproc = myConfig {
+                modMask           = mod4Mask
+              , terminal          = myTerminal
+	      , borderWidth       = 1
+              , focusFollowsMouse = False
+              , workspaces        = map show [1..9]
+              , layoutHook        = myLayoutHook
+              , manageHook        = myManageHook
+              , startupHook       = myStartupHook
+              , logHook           = myXmobarPP xmproc
+              } `additionalKeys` myAdditionalKeys
 
 myXmobarPP xmproc = dynamicLogWithPP $ xmobarPP { ppOutput = hPutStrLn xmproc
                                                 , ppTitle = xmobarColor "white" "" . shorten 60
@@ -55,6 +61,7 @@ myLogHook = fadeInactiveLogHook fadeAmount
 myLayoutHook = mine
     where mine     = avoidStruts $ windowArrange $ 
                      noBorders Full ||| Mirror tiled ||| tiled
+                     ||| Dishes 2 (1/6) ||| spiral (6/7)
           tiled    = Tall 1 (3/100) (1/2)
 
 myManageHook = manageHook myConfig
@@ -72,3 +79,7 @@ myManageHook = manageHook myConfig
 
 
 myStartupHook = setWMName "LG3D"
+
+
+myAdditionalKeys = [ ((mod4Mask .|. shiftMask, xK_b), withFocused toggleBorder)
+                   ]
