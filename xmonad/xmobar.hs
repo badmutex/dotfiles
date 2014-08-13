c6398375f52ec6570cb6d4c0203440abcbcbe438
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2014-08-12 21:05:33 -0400

    import xmonad setup

---
+++ xmonad/xmobar.hs
@@ -0,0 +1,20 @@
+
+
+Config {
+    font = "xft:Fixed-8",
+    bgColor = "#000000",
+    fgColor = "#ffffff",
+    lowerOnStart = True,
+    commands = [
+        Run Weather "KPAO" ["-t","<tempF>F <skyCondition>","-L","64","-H","77","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
+        Run MultiCpu ["-t","Cpu: <autototal>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
+        Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Network "enp2s9" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Date "%a %b %_d %l:%M" "date" 10,
+        Run StdinReader
+    ],
+    sepChar = "%",
+    alignSep = "}{",
+    template = "%StdinReader% }{ %multicpu%   %memory%   %swap%   %enp2s9%   <fc=#FFFFCC>%date%</fc>   %KPAO%"
+}
---
+++ xmonad/xmonad.hs
@@ -0,0 +1,93 @@
+
+import System.IO
+import System.Exit
+import XMonad
+import XMonad.Actions.CycleWS
+import XMonad.Actions.GridSelect
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Spiral
+import XMonad.Layout.Tabbed
+import XMonad.Layout.ThreeColumns
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
+
+
+myTerminal = "terminator"
+myModMask = mod4Mask
+
+
+-- Colors for text and backgrounds of each tab when in "Tabbed" layout.
+tabConfig = defaultTheme {
+    activeBorderColor = "#7C7C7C",
+    activeTextColor = "#CEFFAC",
+    activeColor = "#000000",
+    inactiveBorderColor = "#7C7C7C",
+    inactiveTextColor = "#EEEEEE",
+    inactiveColor = "#000000"
+}
+
+-- Color of current window title in xmobar.
+xmobarTitleColor = "#FFB6B0"
+
+-- Color of current workspace in xmobar.
+xmobarCurrentWorkspaceColor = "#CEFFAC"
+
+myLayout = avoidStruts (
+    Tall 1 (3/100) (1/2) |||
+    Mirror (Tall 1 (3/100) (1/2)) |||
+    tabbed shrinkText tabConfig |||
+    Full |||
+    spiral (6/7))
+
+
+
+myManageHook = manageHook defaultConfig
+
+myGSConfig = defaultGSConfig
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) =
+    M.fromList [
+           ((modm               , xK_Right), nextWS)
+         , ((modm               , xK_Left) , prevWS)
+         , ((modm .|. shiftMask , xK_Right), shiftToNext >> nextWS)
+         , ((modm .|. shiftMask , xK_Left) , shiftToPrev >> prevWS)
+         , ((modm               , xK_g)    , goToSelected myGSConfig)
+         ]
+
+
+defaults =
+    let cfg = defaultConfig
+    in cfg {
+    -- simple stuff
+    terminal           = myTerminal
+  , modMask            = myModMask
+
+  -- hooks, layouts
+  , layoutHook = smartBorders $ myLayout
+
+  -- key bindings
+  , keys = myKeys <+> keys cfg
+
+  }
+
+
+
+main = do
+  xmproc <- spawnPipe "xmobar ~/.xmonad/xmobar.hs"
+  xmonad $  defaults {
+    logHook = dynamicLogWithPP $ xmobarPP {
+      ppOutput = hPutStrLn xmproc
+    , ppTitle = xmobarColor xmobarTitleColor "" . shorten 100
+    , ppCurrent = xmobarColor xmobarCurrentWorkspaceColor ""
+    , ppSep = "    "
+    }
+  , manageHook = manageDocks <+> myManageHook
+  }
+
