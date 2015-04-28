e5d68036a2f7e8e323156f31deff02fc0accd8ba
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2015-04-28 10:37:49 -0400

    whitespace change

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -13,56 +13,56 @@
         virtualenv
       ];
 
-    allPkgs = [
-
-      # web
-      chromium
-
-      # office
-      emacs
-      evince
-      kde4.gwenview
-      dropbox
-      libreoffice
-      inkscape
-      (callPackage ./apps/mendeley.nix {})
-      aspell
-      aspellDicts.en
-      thunderbird
-
-      # science
-      (callPackage ./apps/vmd.nix {})
-
-      # latex
-      texLiveFull
-      biber
-      texmaker
-
-      # misc tools
-      tmux
-      keepass xdotool
-      colordiff
-      unzip
-      spotify
-      xclip
-      yubikey-personalization-gui
-      (callPackage ./apps/wesnoth.nix {})
-
-      # xmonad
-      dmenu
-      trayer
-      haskellPackages.xmobar
-      xcompmgr
-      networkmanagerapplet
-      kde4.kmix
-
-      # nix-related tools
-      nix-repl
-      strategoPackages.strategoxt # provides `pp-aterm` for printing .drv files
-
-     ]
-     ++
-     pythonDevel;
+      allPkgs = [
+  
+        # web
+        chromium
+  
+        # office
+        emacs
+        evince
+        kde4.gwenview
+        dropbox
+        libreoffice
+        inkscape
+        (callPackage ./apps/mendeley.nix {})
+        aspell
+        aspellDicts.en
+        thunderbird
+  
+        # science
+        (callPackage ./apps/vmd.nix {})
+  
+        # latex
+        texLiveFull
+        biber
+        texmaker
+  
+        # misc tools
+        tmux
+        keepass xdotool
+        colordiff
+        unzip
+        spotify
+        xclip
+        yubikey-personalization-gui
+        (callPackage ./apps/wesnoth.nix {})
+  
+        # xmonad
+        dmenu
+        trayer
+        haskellPackages.xmobar
+        xcompmgr
+        networkmanagerapplet
+        kde4.kmix
+  
+        # nix-related tools
+        nix-repl
+        strategoPackages.strategoxt # provides `pp-aterm` for printing .drv files
+  
+       ]
+       ++
+       pythonDevel;
 
      mkEnv = name: paths: buildEnv {
        name = "badi-" + name + "-packages";
