7025d8e9a4c5ac8e6a8e55560086f76a3f236de9
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2015-07-19 00:30:09 -0400

    enable plugins for chromium

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -2,6 +2,12 @@
 
   allowUnfree = true;
 
+  chromium = {
+    enablePepperFlash = true;
+    enablePepperPDF = true;
+    enableWideVine = true;
+  };
+
   packageOverrides = pkgs: with pkgs;
     let
 
