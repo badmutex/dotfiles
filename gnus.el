c8c904c6dd51ba1530f604e57c677f15dd6d40a6
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-01-15 13:35:16 -0500

    gnus: send emails via gmail smtp

--- gnus.el
+++ gnus.el
@@ -11,6 +11,10 @@
 (setq smtpmail-smtp-service 587
       gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
 
+(setq
+ send-mail-function 'smtpmail-send-it
+ smtpmail-smtp-server "smtp.gmail.com")
+
 (setq mm-text-html-renderer 'gnus-w3m)
 
 (setq
