19a6584e8b9ee2a81b53a707bad5d9bdd206106c
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-01-15 13:20:50 -0500

    gnus: more configuration

--- gnus.el
+++ gnus.el
@@ -1,3 +1,7 @@
+
+(setq user-mail-address "abdulwahidc@gmail.com"
+      user-full-name "Badi' Abdul-Wahid")
+
 (setq gnus-select-method
       '(nnimap "gmail"
                (nnimap-address "imap.gmail.com")
@@ -9,7 +13,24 @@
 
 (setq mm-text-html-renderer 'gnus-w3m)
 
-(setq gnus-thread-sort-functions
-      '(gnus-thread-sort-by-date
-        gnus-thread-sort-by-subject
-        gnus-thread-sort-by-score))
+(setq
+ gnus-thread-sort-functions
+ '(gnus-thread-sort-by-number
+   gnus-thread-sort-by-subject
+   gnus-thread-sort-by-score
+   gnus-thread-sort-by-most-recent-date)
+
+ gnus-sum-thread-tree-root "╭▷" ; `>`
+ gnus-sum-thread-tree-false-root "▷" ; `>`
+ ;; gnus-sum-thread-tree-single-indent " " ; ``
+ gnus-sum-thread-tree-vertical "│" ; `| `
+ gnus-sum-thread-tree-indent " " ; `-`
+ gnus-sum-thread-tree-leaf-with-other "├► " ;`+->`
+ gnus-sum-thread-tree-single-leaf "╰► " ; `\->`
+ gnus-user-date-format-alist '(((gnus-seconds-today) . "T   %H:%M")
+                               ((+ 86400 (gnus-seconds-today)) . "Y   %H:%M")
+                               (604800 . "%a %H:%M")
+                               ((gnus-seconds-month) . "%a %d")
+                               ((gnus-seconds-year) . "%b %d")
+                               (t . "%b %d %Y"))
+ gnus-summary-line-format "%U%R%z %-12&user-date; %(%[%-42,42n%]:%) %B %s\n")
