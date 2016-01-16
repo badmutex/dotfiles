9130ed3856e053601bb1df7c38b8c3fe292e63d3
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-01-15 22:38:17 -0500

    nixpkgs: add megatools

--- nixpkgs/cloudfs.nix
+++ nixpkgs/cloudfs.nix
@@ -1,5 +1,6 @@
 { pkgs, stdenv
 , withDropbox ? stdenv.isLinux
+, withMega ? true
 , ...}:
 
 with pkgs;
@@ -8,3 +9,4 @@ let
   inherit (stdenv.lib) optional;
 in
 optional withDropbox dropbox
+++ optional withMega megatools
