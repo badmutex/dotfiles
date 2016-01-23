2a68ddd4a2190fe7a7a63f9d8a882835fb62d5c9
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-01-23 12:34:52 -0500

    gnus: fix authinfo.gpg problems

--- gnus.el
+++ gnus.el
@@ -2,11 +2,21 @@
 (setq user-mail-address "abdulwahidc@gmail.com"
       user-full-name "Badi' Abdul-Wahid")
 
-(setq gnus-select-method
-      '(nnimap "gmail"
-               (nnimap-address "imap.gmail.com")
-               (nnimap-server-port "imaps")
-               (nnimap-stream ssl)))
+;; (setq gnus-select-method 
+;;       '(nnmaildir "gmail"
+;;                   (directory "~/.maildir/abdulwahidc@gmail.com/")))
+
+(setq gnus-select-method '(nnimap "gmail"
+                                  (nnimap-address "imap.gmail.com")
+                                  (nnimap-server-port "imaps")
+                                  (nnimap-stream ssl))
+
+      ;; where we store the password
+      ;; nntp-authinfo-file "~/.authinfo.gpg"
+      ;; auth-source-debug t
+
+      ;; agent seems to confuse nnimap
+      gnus-agent nil)
 
 (setq smtpmail-smtp-service 587
       gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
@@ -41,7 +51,7 @@
  gnus-summary-line-format "%U%R%z %-12&user-date; %(%[%-42,42n%]:%) %B %s\n")
 
 
-(add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
+;; (add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
 
 ;; FIXME: org-mime not working yet
 (el-get-bundle org-mime)
