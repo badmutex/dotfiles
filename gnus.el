4fe9ec730ae267b2d2143786c6b98dbc17183aa9
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-01-30 13:31:43 -0500

    gnus: cleanup

--- gnus.el
+++ gnus.el
@@ -13,6 +13,7 @@
  ;; the correct From header to be determined in the reply.
  gnus-posting-styles
  '((".*"
+    (address "abdulwahidc@gmail.com")
     (signature "Badi' Abdul-Wahid"))
    ((header "To" ".*badi@iu\\.edu.*")
     (address "badi@iu.edu")
@@ -37,18 +38,29 @@
  )
 
 
-(setq gnus-select-method '(nnimap "gmail"
-                                  (nnimap-address "imap.gmail.com")
-                                  (nnimap-server-port "imaps")
-                                  (nnimap-stream ssl)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; mail sources
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq
+
+ gnus-select-method
+ '(nnimap "gmail"
+          (nnimap-address "imap.gmail.com")
+          (nnimap-server-port "imaps")
+          (nnimap-stream ssl))
+
+ gnus-secondary-select-methods
+ '((nnimap "iu"
+           (nnimap-address "imap.exchange.iu.edu")
+           (nnimap-server-port "imaps")
+           (nnimap-stream ssl))))
 
 
-(setq gnus-secondary-select-methods '((nnimap "iu"
-                                              (nnimap-address "imap.exchange.iu.edu")
-                                              (nnimap-server-port "imaps")
-                                              (nnimap-stream ssl))))
-
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; gnus configuration
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (setq
  ;; where we store the password
@@ -61,55 +73,52 @@
  ;; smtpmail-smtp-service 587
  gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]"
 
- )
+ mm-text-html-renderer 'shr
+ ;; SHR rendering is pretty terrible with a dark theme
+ ;; https://emacs.stackexchange.com/questions/2955
+ ;; https://lists.gnu.org/archive/html/help-gnu-emacs/2014-07/msg00243.html
+ shr-color-visible-distance-min 50
+ shr-color-visible-luminance-min 70
 
 
-(setq mm-text-html-renderer 'shr
-      ;; SHR rendering is pretty terrible with a dark theme
-      ;; https://emacs.stackexchange.com/questions/2955
-      ;; https://lists.gnu.org/archive/html/help-gnu-emacs/2014-07/msg00243.html
-      shr-color-visible-distance-min 50
-      shr-color-visible-luminance-min 70
+ gnus-thread-sort-functions '(gnus-thread-sort-by-number
+                              gnus-thread-sort-by-subject
+                              gnus-thread-sort-by-score
+                              gnus-thread-sort-by-most-recent-date)
+ gnus-use-adaptive-scoring '(word line)
+ gnus-default-adaptive-score-alist '((gnus-unread-mark)
+                                     (gnus-ticked-mark (from 4))
+                                     (gnus-dormant-mark (from 5))
+                                     (gnus-del-mark (from -4) (subject -1))
+                                     (gnus-read-mark (from 4) (subject 2))
+                                     (gnus-expirable-mark (from -1) (subject -1))
+                                     (gnus-killed-mark (from -1) (subject -3))
+                                     (gnus-kill-file-mark)
+                                     (gnus-ancient-mark)
+                                     (gnus-low-score-mark)
+                                     (gnus-catchup-mark (from -1) (subject -1)))
 
+ ;; hide citations by default since I read by thread
+ gnus-treat-hide-citation t
 
-      gnus-thread-sort-functions '(gnus-thread-sort-by-number
-                                   gnus-thread-sort-by-subject
-                                   gnus-thread-sort-by-score
-                                   gnus-thread-sort-by-most-recent-date)
-      gnus-use-adaptive-scoring '(word line)
-      gnus-default-adaptive-score-alist '((gnus-unread-mark)
-                                          (gnus-ticked-mark (from 4))
-                                          (gnus-dormant-mark (from 5))
-                                          (gnus-del-mark (from -4) (subject -1))
-                                          (gnus-read-mark (from 4) (subject 2))
-                                          (gnus-expirable-mark (from -1) (subject -1))
-                                          (gnus-killed-mark (from -1) (subject -3))
-                                          (gnus-kill-file-mark)
-                                          (gnus-ancient-mark)
-                                          (gnus-low-score-mark)
-                                          (gnus-catchup-mark (from -1) (subject -1)))
+ ;; don't break threads when entering groups with new messages
+ gnus-fetch-old-headers 'some
 
-      ;; hide citations by default since I read by thread
-      gnus-treat-hide-citation t
-
-      ;; don't break threads when entering groups with new messages
-      gnus-fetch-old-headers 'some
-
-      ;; see C-u C-x = for information on character under cursor
-      gnus-sum-thread-tree-root "╭▷" ; `>`
-      gnus-sum-thread-tree-false-root "▷" ; `>`
-      ;; gnus-sum-thread-tree-single-indent " " ; ``
-      gnus-sum-thread-tree-vertical "│" ; `| `
-      gnus-sum-thread-tree-indent " " ; `-`
-      gnus-sum-thread-tree-leaf-with-other "├► " ;`+->`
-      gnus-sum-thread-tree-single-leaf "╰► " ; `\->`
-      gnus-user-date-format-alist '(((gnus-seconds-today) . "T   %H:%M")
-                                    ((+ 86400 (gnus-seconds-today)) . "Y   %H:%M")
-                                    (604800 . "%a %H:%M")
-                                    ((gnus-seconds-month) . "%a %d")
-                                    ((gnus-seconds-year) . "%b %d")
-                                    (t . "%b %d %Y"))
-      gnus-summary-line-format "%U%R%z %-12&user-date; %(%[%-42,42n%]:%) %B %s\n")
+ ;; see C-u C-x = for information on character under cursor
+ gnus-sum-thread-tree-root "╭▷" ; `>`
+ gnus-sum-thread-tree-false-root "▷" ; `>`
+ ;; gnus-sum-thread-tree-single-indent " " ; ``
+ gnus-sum-thread-tree-vertical "│" ; `| `
+ gnus-sum-thread-tree-indent " " ; `-`
+ gnus-sum-thread-tree-leaf-with-other "├► " ;`+->`
+ gnus-sum-thread-tree-single-leaf "╰► " ; `\->`
+ gnus-user-date-format-alist '(((gnus-seconds-today) . "T   %H:%M")
+                               ((+ 86400 (gnus-seconds-today)) . "Y   %H:%M")
+                               (604800 . "%a %H:%M")
+                               ((gnus-seconds-month) . "%a %d")
+                               ((gnus-seconds-year) . "%b %d")
+                               (t . "%b %d %Y"))
+ gnus-summary-line-format "%U%R%z %-12&user-date; %(%[%-42,42n%]:%) %B %s\n")
 
 
 
