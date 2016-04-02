12735dafa5d5c78a65d06ec08b1f6bd6568bc20e
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-04-01 22:30:39 -0400

    backup fangorn xmonad

---
+++ fangorn.bak/xmonad/xmobar.hs
@@ -0,0 +1,21 @@
+
+
+Config {
+    position = Top,
+    font = "xft:Fixed-11",
+    bgColor = "#000000",
+    fgColor = "#ffffff",
+    lowerOnStart = True,
+    commands = [
+        Run Weather "KBMG" ["-t","<tempF>F <skyCondition>","-L","64","-H","77","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
+        Run MultiCpu ["-t","Cpu: <autototal>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
+        Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run DynNetwork ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Date "%a %b %_d %l:%M" "date" 10,
+        Run StdinReader
+    ],
+    sepChar = "%",
+    alignSep = "}{",
+    template = "%StdinReader% }{ %multicpu%   %memory%   %swap%  %dynnetwork%   <fc=#FFFFCC>%date%</fc>   %KBMG%"
+}
