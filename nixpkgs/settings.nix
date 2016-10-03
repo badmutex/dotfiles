ead6cfb74565d0cfcfbbf8ea5ec8a7345c416031
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-10-03 15:36:57 -0400

    nixpkgs: enable plugins for firefox and chrome

--- nixpkgs/settings.nix
+++ nixpkgs/settings.nix
@@ -2,6 +2,17 @@
 
   allowUnfree = true;
 
+  firefox = {
+    enableGoogleTalkPlugin = true;
+    enableAdobeFlash = true;
+  };
+
+  chrome = {
+    enablePepperFlash = true;
+    enablePepperPDF = true;
+    enableWideVine = true;
+  };
+
   chromium = {
     enablePepperFlash = true;
     enablePepperPDF = true;
