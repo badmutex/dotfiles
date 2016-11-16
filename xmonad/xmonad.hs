3e942276c221bd46d8ed5d2ef90c66a00abd10ae
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-11-16 10:00:03 -0500

    xmonad: mod+shift+b changes to random background

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -106,6 +106,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) =
   , ((modm .|. shiftMask , xK_Left) , shiftToPrev >> prevWS)
   , ((modm               , xK_g)    , goToSelected myGSConfig)
   , ((modm               , xK_b)    , spawn "google-chrome-stable")
+  , ((modm .|. shiftMask , xK_b)    , spawn "feh --no-fehbg --randomize --bg-scale ~/Backgrounds")
   , ((noModMask, xF86XK_MonBrightnessUp)    , spawn "xbacklight +20")
   , ((noModMask, xF86XK_MonBrightnessDown)  , spawn "xbacklight -20")
   , ((noModMask, xF86XK_AudioRaiseVolume) , spawn "amixer set 'Master' 1%+")
