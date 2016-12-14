0edd85e03a626bd06d771ba5826f9e6d5c173246
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-12-14 17:15:29 -0500

    emacs: move custom variables to ~/.emacs-custom.el

--- emacs.el
+++ emacs.el
@@ -766,23 +766,8 @@
 ;; custom vars
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (nil shakespeare-mode oauth2 nixos-options let-alist csv-mode ac-haskell-process)))
- '(safe-local-variable-values
-   (quote
-    ((haskell-process-use-ghci . t)
-     (haskell-indent-spaces . 4)
-     (enforce-mode)
-     (column-epa-armor . t)
-     (org-confirm-babel-evaluate)
-     (epa-armor . t)))))
-
+(setq custom-file "~/.emacs-custom.el")
+(load custom-file)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; keybindings
